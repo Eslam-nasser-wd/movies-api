cb394d895cc335ffbc4225bb887b14da36ed14c8
eslamNasser <eslamnasser@hmaserv.net>
2017-02-01 00:08:49 +0200

    test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    //'db': 'mongodb://root:94982bf5-85f7-436f-9058-28357aa23e3a@ds135669.mlab.com:35669/movies_api' // mLab
-    'db2': 'mongodb://root:94982bf5-85f7-436f-9058-28357aa23e3a@jello.modulusmongo.net:27017/zy8dinYp' // Xervo
+    'db': 'mongodb://root:94982bf5-85f7-436f-9058-28357aa23e3a@ds135669.mlab.com:35669/movies_api' // mLab
+    //'db2': 'mongodb://root:94982bf5-85f7-436f-9058-28357aa23e3a@jello.modulusmongo.net:27017/zy8dinYp' // Xervo
     
 }
